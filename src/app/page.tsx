ba08e1aa479e8e3671e5bdb49824db498a6236d2
Tivakorn <50856896+tivakorn@users.noreply.github.com>
2024-02-09 02:02:32 +0700

    add fix messsages.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
 
   return (
-    <main className="flex h-screen flex-col items-center justify-center p-4 md:px-24 py-32 gap-4">
+    <main className="flex h-screen flex-col items-center justify-center p-4 md:px-24 gap-4">
 
       <div className="z-10 border rounded-lg max-w-5xl w-full h-full text-sm lg:flex">
         <ChatLayout defaultLayout={defaultLayout} navCollapsedSize={8} />
