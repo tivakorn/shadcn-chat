f068e29ae55b44cfbe26fe0587db32ac47474690
Tivakorn <50856896+tivakorn@users.noreply.github.com>
2024-02-09 02:11:02 +0700

    🔧 chore(.gitignore): add ignore rules for auto-generated PWA files ✨ feat(next.config.mjs): configure Next.js with Next PWA plugin for Progressive Web App support 📦 feat(package.json): add next-pwa package as a dependency 📝 docs(public/manifest.json): add manifest file for PWA configuration 🔧 chore(src/app/layout.tsx): add metadata and viewport configuration for PWA support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,19 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Shadcn Chat",
   description: "Chat/message components for Shadcn",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "nextjs13", "next13", "pwa", "next-pwa"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [
+    { name: "Tivkorn Lubkim" },
+  ],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
+    { rel: "icon", url: "icons/icon-128x128.png" },
+  ],
 };
 
 export const viewport = {
